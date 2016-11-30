afe1d865cbe4390d9ce2e5652d30a8f399044fed
Chris Campbell <campbellC@users.noreply.github.com>
2016-11-30 18:01:54 +0000

    moved pomodoro to its own repo

--- cropBook/cropBook.sh
+++ cropBook/cropBook.sh
@@ -1,11 +1,11 @@
-/opt/local/bin/python /Users/chrisCampbell/Dropbox/UsefulScripts/cropBook/splitter.py $@
+/usr/bin/python /Users/chrisCampbell/Dropbox/UsefulScripts/cropBook/splitter.py $@
 for var in $@
 do
 	name=`echo $var | rev | cut -c 5- | rev`
 	echo $name
 	java -jar /Applications/briss/briss-0.9.jar -s "${name}Even.pdf" -d "${name}EC.pdf"
 	java -jar /Applications/briss/briss-0.9.jar -s "${name}Odd.pdf" -d "${name}OC.pdf"
-	/opt/local/bin/python /Users/chrisCampbell/Dropbox/UsefulScripts/cropBook/joiner.py "${name}EC.pdf" "${name}OC.pdf" 
+	/usr/bin/python /Users/chrisCampbell/Dropbox/UsefulScripts/cropBook/joiner.py "${name}EC.pdf" "${name}OC.pdf" 
 	rm "${name}EC.pdf" "${name}OC.pdf" "${name}Even.pdf" "${name}Odd.pdf"
 done 
 	
--- pomodoroTimer/alarm.py
+++
@@ -1,6 +0,0 @@
-import soundfile
-import sounddevice
-class Alarm:
-    def playAlarm(self):
-        data, fs = soundfile.read("/System/Library/Sounds/Glass.aiff")
-        sounddevice.play(data, fs, blocking=True)
